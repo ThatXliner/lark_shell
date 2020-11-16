d28f2248aebfcdecf832718d55de0de1f7dc2e34
Bryan Hu <bryan.hu.2020@gmail.com>
2020-11-16 11:43:15 -0800

    :bulb: :art: :memo: Added comments and docs
    It's not a library so a lot of things are left out

--- lark_shell.py
+++ lark_shell.py
@@ -13,46 +13,57 @@ An implementation of https://lark-parser.github.io/lark/ide/app.html in the term
 For playing with Lark.
 
 """
+# TODO: Add command-line arguments and argparse support
 import sys
 
 import lark
 import urwid
 
-output = urwid.SelectableIcon(("wait", "Output waiting..."))
+output = urwid.SelectableIcon(("wait", "Output waiting..."))  # Initially wait for input
 meta = urwid.SelectableIcon(("success", f"Lark-parser, version {lark.__version__}"))
 
 
-def update_grammar(_, new_text):
+def _update_grammar(_, new_text: str) -> None:
+    """Update the UI when the grammar changes."""
     _parse(new_text, grammar_input.get_edit_text(), output)
 
 
-def _parse(grammar, parse_input, output_obj):  # Lark parse here
+def _parse(grammar: str, parse_input: str, output_obj: urwid.Text) -> None:
+    """Update the UI with the given grammar and input."""
     try:
+        # Create a parser with "earley"
         parser = lark.Lark(grammar, parser="earley", lexer="auto")
+        # Generate a tree
         tree = parser.parse(parse_input)
 
     except (lark.exceptions.GrammarError, TypeError):
         output_obj.set_text(("error", "Incomplete grammar!"))
+
     except (lark.exceptions.UnexpectedToken, lark.exceptions.UnexpectedEOF) as e:
         output_obj.set_text(("error", str(e)))
-    except FileNotFoundError:
+
+    except FileNotFoundError:  # The relative imported file was not found
         output_obj.set_text(("error", "File not found"))
 
-    except Exception as e:  # Everything else...
+    # NOTE(ThatXliner): We're doing this for now until we can
+    # get all the expected exceptions
+    except Exception as e:  # XXX
         output_obj.set_text(("error", repr(e)))
-    else:
+
+    else:  # Parsing succeeded
         output_obj.set_text(("success", str(tree.pretty())))
 
 
-def update_input(_, new_input):
+def _update_input(_, new_input: str) -> None:
+    """Update the UI when the input changes."""
     _parse(lark_grammar.get_edit_text(), new_input, output)
 
 
 lark_grammar = urwid.Edit(("bold", "Grammar\n"), multiline=True, allow_tab=True)
 grammar_input = urwid.Edit(("bold", "Grammar input\n"), multiline=True, allow_tab=True)
 
-urwid.connect_signal(lark_grammar, "change", update_grammar)
-urwid.connect_signal(grammar_input, "change", update_input)
+urwid.connect_signal(lark_grammar, "change", _update_grammar)
+urwid.connect_signal(grammar_input, "change", _update_input)
 
 UI = [
     urwid.LineBox(lark_grammar),
@@ -62,17 +73,25 @@ UI = [
     urwid.Divider(div_char="-"),
     meta,
 ]
-main_ui = urwid.Filler(urwid.Pile(UI))
 PALLETE = [
     ("bold", "bold", ""),
     ("wait", "yellow", ""),
     ("error", "light red", ""),
     ("success", "light green", ""),
 ]
-loop = urwid.MainLoop(main_ui, palette=PALLETE)
+
+loop = urwid.MainLoop(urwid.Filler(urwid.Pile(UI)), palette=PALLETE)
 
 
-def main():
+def main() -> None:
+    """The main CLI entry point.
+
+    Returns
+    -------
+    None
+        This function does not return anything.
+
+    """
     try:
         loop.run()
     except KeyboardInterrupt:
